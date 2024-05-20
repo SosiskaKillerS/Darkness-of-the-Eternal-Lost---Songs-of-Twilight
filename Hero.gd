2e455550160a1846152ac919c2c1999f95935ac8
Artem Polikarpov <Artemijepolikarpov@yandex.ru>
2024-05-20 19:55:40 +0300

    update 1
    animation + moving + music

--- Hero.gd
+++ Hero.gd
@@ -2,11 +2,27 @@ extends CharacterBody2D
 
 @export var speed = 400 # Скорость персонажа
 
-func get_input(): # Функция, чтобы получить направление
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+@onready var animation_player = $AnimationPlayer #чтобы удобней писать 
 
-func _physics_process(delta): # Осущенствленеие и сглаживание перемещения 
+func get_input(): # Функция, чтобы получить направление
+	var input_direction = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
+	velocity = input_direction * speed # скорость = скорость * направление движения 
+
+func player_animation_update(): # функция для анимации передвижения персонажа 
+	if velocity.length() == 0: # Если скорость = 0 возвращает анимацию на первый кадр 
+		animation_player.stop() # Остановка анимации персонажа
+	else:
+		var direction = "down"
+		if velocity.x < 0: direction = "left"
+		elif velocity.x > 0: direction = "right"
+		elif velocity.y < 0: direction = "up" 
+	
+		animation_player.play("walk"+direction) # проигрыватель который примет строчку "walk"+направление
+
+
+func _physics_process(delta): # Осуществление
 	get_input()
 	move_and_slide()
+	player_animation_update()
+
 
