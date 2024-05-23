6d11daad2e2d13c2a536a67ea2a7cc2d99f2f107
Artem Polikarpov <uka.blyatsuka@yandex.ru>
2024-05-23 17:55:54 +0300

    Main Manu Update 1
    Ver 1 of MM

---
+++ main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+func _on_continue_game_pressed():
+	get_tree().change_scene_to_file("res://level_1.tscn")
+
+
+func _on_new_game_pressed():
+	pass
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://Options.tscn")
+
+
+func _on_authors_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
