50eed03a9de4bcc55f66b651821eb31d83655e54
nick/ga <158477658+SosiskaKillerS@users.noreply.github.com>
2024-05-31 00:21:39 +0300

    Update TileMap ver 1.2 + Skeleton Update ver 1.1 + SceneManager
    Update map design and Skeleton work and add some plagin for smooth transitions between sceens

--- main_menu.gd
+++ main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_continue_game_pressed():
-	get_tree().change_scene_to_file("res://level_1.tscn")
+	get_tree().change_scene_to_file("res://game.tscn")
 
 
 func _on_new_game_pressed():
