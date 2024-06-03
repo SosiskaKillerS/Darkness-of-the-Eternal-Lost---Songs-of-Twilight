31c065b4edeed57b49eed2bcd96979dab4b641a9
nick/ga <158477658+SosiskaKillerS@users.noreply.github.com>
2024-06-03 08:07:41 +0300

    Battle music
    New battle music for skeleton battle
    
    Co-Authored-By: dmntrx <145675897+dmntrx@users.noreply.github.com>

---
+++ skeleton_battle.gd
@@ -0,0 +1,25 @@
+extends Control
+
+signal textbox_closed
+func _ready():
+	$TextBox.hide()
+	$ActionPanel.hide()
+	
+	display_text("Somebody wants ice cream !")
+	await textbox_closed
+	$ActionPanel.show() 
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and $TextBox.visible:
+		$TextBox.hide() 
+		emit_signal("textbox_closed")
+func display_text(text):
+	$TextBox.show()
+	$TextBox/Text.text = text
+	
+var new_pistion = Vector2(923,-965)
+
+func _on_run_button_pressed():
+	display_text("You understand that it is time to run.")
+	await textbox_closed
+	get_tree().change_scene_to_file("res://game.tscn")
+	$Player/Player.position = new_pistion
