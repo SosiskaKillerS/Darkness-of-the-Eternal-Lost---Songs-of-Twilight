ba16543f2cd9e84e22c93ade0d2218e7a2537d65
nick/ga <158477658+SosiskaKillerS@users.noreply.github.com>
2024-06-09 20:00:06 +0300

    Update battle

--- skeleton_battle.gd
+++ skeleton_battle.gd
@@ -1,14 +1,25 @@
 extends Control
+
 @onready var battle = $"."
 @onready var player = $"../../Player/Player"
+
+
 signal textbox_closed
+
+
 func _ready():
+	set_health($PlayerPanel/PlayerData/ProgressBar, State.health, State.max_health)
 	$TextBox.hide()
-	$ActionPanel.hide()	
-	
+	$ActionPanel.hide()
 	display_text("It was looked better from afar...")
 	await textbox_closed
 	$ActionPanel.show() 
+	
+func set_health(progress_bar, health, max_health):
+	progress_bar.value = health
+	progress_bar.max_value = max_health
+	progress_bar.get_node("Label").text = "%d/%d" % [health, max_health]
+
 func _input(event):
 	if Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and $TextBox.visible:
 		$TextBox.hide() 
